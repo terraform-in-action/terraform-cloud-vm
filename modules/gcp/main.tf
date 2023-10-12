651513805671332439a85d5b9c8990cc96326f49
cschar <cschar@users.noreply.github.com>
2023-10-11 22:00:55 -0400

    Update ubuntu version to 2204

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -37,7 +37,7 @@ resource "google_compute_instance" "compute_instance" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
